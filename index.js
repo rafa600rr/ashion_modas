efaea56165aab457407cd2f44df5c76567d9e09a
Rafael Vieira <rafaelvieirapalmital@outlook.com>
2020-09-28 15:01:49 -0300

    Editando arquivos CSS, JS; para se adequaderm ao HANDLEBARS

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 // Importar express
 let express = require('express');
 
-// Importar o express-handlebars
+// Importar handlebars
 let handlebars = require('express-handlebars');
 
-// App
+// Exportar APP
 let app = express();
 
-// Templete
+// Templetes para handlebars
 app.engine('handlebars', handlebars({defaultLayout:'principal'}));
 app.set('view engine', 'handlebars');
 
+// Diretório public
+app.use(express.static(__dirname + '/public')); 
+
 // Rotas
-app.get('/index', (req, res) => {
+app.get('/index', (req, res) =>{
     res.render('index');
-});
+})
 
 // Servidor
 app.listen(8080);
