ce46efcbceafe4da8640d00e58b352978c006939
Rafael Vieira <rafaelvieirapalmital@outlook.com>
2020-10-01 12:59:00 -0300

    Crinado meu JSON de cadastro

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let express = require('express');
 // Importar handlebars
 let handlebars = require('express-handlebars');
 
+// Importar Body P\rser
+let bodyParser = require('body-parser');
+
 // Exportar APP'
 let app = express();
 
@@ -13,6 +16,13 @@ app.set('view engine', 'handlebars');
 
 // Diretório public
 app.use(express.static(__dirname + '/public')); 
+
+// JSON via URL
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// JSON de clientes
+let clientes = [];
 
 // Rotas
 app.get('/inicio', (req, res) =>{
@@ -28,6 +38,13 @@ app.get('/login', (req, res) =>{
 app.get('/cadastro', (req, res) =>{
     res.render('cadastro');
 });
+
+app.post('/cadastro', (req, res) =>{
+    clientes.push({"nome": require.body.nome, 
+    "sobrenome": require.body.sobreNome})
+    console.log(clientes)
+    res.render('/cadastro')
+})
 
 // Servidor
 app.listen(8080);
